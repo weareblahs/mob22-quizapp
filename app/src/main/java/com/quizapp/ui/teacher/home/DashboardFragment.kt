2871185d9feafb8bd2694e460a19eb7001fc2b68
limyaoqi <limyaoqi@gmail.com>
2025-04-15 15:15:55 +0800

    Setup Project Structure

--- app/src/main/java/com/quizapp/ui/teacher/home/DashboardFragment.kt
+++ app/src/main/java/com/quizapp/ui/teacher/home/DashboardFragment.kt
@@ -1,19 +1,17 @@
-package com.quizapp.ui.home
+package com.quizapp.ui.teacher.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.quizapp.R
 import com.quizapp.databinding.FragmentHomeBinding
 import com.quizapp.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragment() {
-    override val viewModel: HomeViewModel by viewModels()
+class DashboardFragment : BaseFragment() {
+    override val viewModel: DashboardViewModel by viewModels()
     private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
